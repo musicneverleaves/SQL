9311e730a4d18bb9216ee0c7be0f662b5a9a5e90
Pi Chin Chao <10030812peter@gmail.com>
2020-03-01 03:26:34 -0700

    Delete exec vendorname+table func.sql

--- exec vendorname+table func.sql
+++
@@ -1,8 +0,0 @@
-SELECT Vendors.VendorName,fnTable.*
-from Vendors
-join
-Invoices
-on Vendors.VendorID = Invoices.VendorID
-join
-fnDateRange(20111210,20111220) AS fnTable
-on Invoices.InvoiceNumber = fnTable.InvoiceNumber;
